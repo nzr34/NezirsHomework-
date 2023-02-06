fb535bbfd05d8b2f56099268ddb5df483e1e229d
nzr34 <nezirgokce@gmail.com>
2023-02-05 20:49:43 -0500

    HW Feb 5th

---
+++ src/Homework15/PartTimeTester.java
@@ -0,0 +1,10 @@
+package Homework15;
+
+public class PartTimeTester {
+    public static void main(String[] args) {
+        PartTimeEmployees emp2 = new PartTimeEmployees("David", "TechnicalAssistant", 80000, false);
+        emp2.printInfo();
+        emp2.printBenefits();
+
+    }
+}
---
+++ src/Homework15/Shape.java
@@ -0,0 +1,32 @@
+package Homework15;
+
+public class Shape {
+    double radius;
+    static double piValue=3.14;
+    Shape(double radius ){
+        this.radius=radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
+class Circle extends Shape{
+    Circle(double radius){
+        super( radius);
+
+
+    }
+    void areaOfCircle( ){
+        System.out.println("The area of circle is ="+(radius*radius)*piValue);
+
+    }
+
+
+    public static void main(String[] args) {
+        Circle newC=new Circle(7);
+        newC.areaOfCircle();
+
+    }
+
+}
